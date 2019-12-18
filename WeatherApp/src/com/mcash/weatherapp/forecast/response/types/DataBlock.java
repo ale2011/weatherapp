ddcc3b54b72c516018d3ecfb3404e1a3a7199e30
u <u@10.0.0.20>
2019-12-18 16:34:17 -0500

    Response - 003

--- WeatherApp/src/com/mcash/weatherapp/forecast/response/types/DataBlock.java
+++ WeatherApp/src/com/mcash/weatherapp/forecast/response/types/DataBlock.java
@@ -14,63 +14,68 @@ import org.json.JSONObject;
  * @author u
  */
 public class DataBlock {
-    
+
     private String summary;
     private String icon;
+    private String time;
     private ForecastData[] data;
-    
-    public DataBlock(JSONObject forecastBlockJsonObject) {
-            try {
-                    summary = forecastBlockJsonObject.getString("summary");
-            } catch (JSONException e) {
+
+    public DataBlock(JSONObject jsonObj) {
+        try {
+            if(jsonObj.has("time"))
+                time = jsonObj.get("time").toString();
+            
+            if(jsonObj.has("summary"))
+                summary = jsonObj.getString("summary"); 
+            
+            if(jsonObj.has("icon"))
+                icon = jsonObj.getString("icon");
+            
+            if(jsonObj.has("data"))
+            {
+                JSONArray blockData = jsonObj.getJSONArray("data");
+                int blockDataLength = blockData.length();
+                data = new ForecastData[blockDataLength];
+                for (int i = 0; i < blockDataLength; i++) 
+                {
+                    ForecastData d = new ForecastData(blockData.getJSONObject(i));
+                    data[i] = d;
+                }
             }
-            try {
-                    icon = forecastBlockJsonObject.getString("icon");
-            } catch (JSONException e) {
-            }
-            try {
-                    JSONArray blockData = forecastBlockJsonObject.getJSONArray("data");
-                    int blockDataLength = blockData.length();
-                    data = new ForecastData[blockDataLength];
-                    for(int i=0; i<blockDataLength; i++) {
-                            ForecastData d = new ForecastData(blockData.getJSONObject(i));
-                            data[i] = d;
-                    }
-            } catch (JSONException e) {
-            }
+        }
+        catch (JSONException e) {
+        }
     }
-    
+
     public String getValue(String key) {
-		if(key.equals(new String("summary"))) {
-			return summary;
-		}
-		else if(key.equals(new String("icon"))) {
-			return icon;
-		}
-		return null;
-	}
-	
-	/**
-	 * method returns the summary for this data block;
-	 * @return string value of summary
-	 */
-	public String summary() {
-		return summary;
-	}
-	
-	/**
-	 * method returns the icon for this data block;
-	 * @return string value of icon
-	 */
-	public String icon() {
-		return icon;
-	}
-	
-	public ForecastData[] data() {
-		return data;
-	}
-    
-    
-    
-    
+        if (key.equals("summary")) {
+            return summary;
+        } 
+        else if (key.equals("icon")) {
+            return icon;
+        } 
+        else if (key.equals("time"))
+        {
+            return time;
+        } 
+        else
+        {
+            
+        }
+        
+        return null;
+    }
+
+    public String summary() {
+        return summary;
+    }
+
+    public String icon() {
+        return icon;
+    }
+
+    public ForecastData[] data() {
+        return data;
+    }
+
 }
